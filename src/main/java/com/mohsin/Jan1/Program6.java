1a4bd5439cdc136ace20a1fc6f3bfcbe08166508
Mohsin <mdmohsinhaider6@gmail.com>
2026-01-08 02:17:31 +0530

    Jan1 Complete

--- src/main/java/com/mohsin/Jan1/Program6.java
+++ src/main/java/com/mohsin/Jan1/Program6.java
@@ -2,16 +2,15 @@ package com.mohsin.Jan1;
 
 import java.util.Scanner;
 
-public class Program5 {
-    // Write a program to check whether a number is positive or negative
+public class Program6 {
+    // Write a program to check whether a number is even or odd.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(checkNumber(scanner));
     }
 
     private static String checkNumber(Scanner scanner){
-        System.out.println("Enter the Number");
-        double integer = scanner.nextDouble();
-        return (integer >= 0)?"Positive Number":"Negative Number";
+        System.out.println("Enter the number");
+        return (scanner.nextDouble()%2 == 0)?"Even":"Odd";
     }
 }
